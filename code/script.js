d9628595513131870dc6c5ab06897b2affd94af6
linnsan2000 <31280120+linnsan2000@users.noreply.github.com>
2017-11-24 14:45:51 +0100

    Timer function

---
+++ code/script.js
@@ -0,0 +1,7 @@
+document.links.forEach = Array.prototype.forEach;
+document.links.forEach(function (element) {
+  const today = new Date()
+  if (element.id > today.getDate()) {
+    element.href = '#';
+  }
+});

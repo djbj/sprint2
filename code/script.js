dd5d4dac437a06e669a916582e36a1c306bfbd64
Bjorn <bjossijohnson@gmail.com>
2017-11-29 14:28:24 +0100

    day

--- code/script.js
+++ code/script.js
@@ -1,7 +1,12 @@
 document.links.forEach = Array.prototype.forEach;
 document.links.forEach(function (element) {
   const today = new Date()
-  if (element.id < today.getDate()) {
+  // if (element.id < today.getDate()) {
+  if (element.id < 1) {
     element.href = '#';
   }
 });
+
+
+
+elem
